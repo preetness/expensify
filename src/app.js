8e7adca59bfd3cfb0e73001096f4500cc50a918c
Preet Dha <manpreetdha+github@gmail.com>
2018-02-27 14:31:39 -0800

    Install react-router-dom, and setup several routes, with corresponding stateless functional components

--- src/app.js
+++ src/app.js
@@ -1,7 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
+import {BrowserRouter, Route} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>This is my boilerplate </p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+	<div>This is from my dashboard component.</div>
+);
+
+const AddExpensePage = () => (
+	<div>This is from my add expense page.</div>
+);
+
+const EditExpensePage = () => (
+	<div>This is from my edit expense page.</div>
+);
+
+const HelpPage = () => (
+	<div>This is from my help page.</div>
+);
+
+const routes = (
+		<BrowserRouter>
+		<div>
+			<Route path="/" component={ExpenseDashboardPage} exact={true} />
+			<Route path="/create" component={AddExpensePage} />
+			<Route path="/edit" component={EditExpensePage}/>
+			<Route path="/help" component={HelpPage}/>
+		</div>
+			
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
